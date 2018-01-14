b59ac2b180a3e25bcbd812530bae087954ecc00a
Kai Richard Koenig <kai.koenig@paessler.com>
2018-01-14 17:57:53 +0100

    Initial commit

---
+++ endpoint/cli.py
@@ -0,0 +1,60 @@
+import sys
+import click
+import requests
+import os
+
+CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
+
+class EndpointAPI(object):
+    def __init__(self, client, auth):
+        self.url = "http://{}/{}".format(os.environ.get("HW_API"), "endpoints")
+        self.client = client
+        self.default_headers = {"X-PA-AUTH-TOKEN": auth}
+
+    def post(self, data, **kwargs):
+        import pudb; pudb.set_trace()  # XXX BREAKPOINT
+        return self.client.post(self.url, data=data, headers=self.get_headers(**kwargs))
+
+    def delete(self, name, **kwargs):
+        return self.client.delete("{}/{}".format(self.url, name), headers=self.get_headers(**kwargs))
+
+    def get_headers(self, **kwargs):
+        headers = kwargs.get("headers", self.default_headers.copy())
+        defaults = self.default_headers.copy()
+        defaults.update(headers)
+        return defaults
+
+@click.group(context_settings=CONTEXT_SETTINGS)
+@click.version_option(version='0.0.1')
+def hw():
+    pass
+
+
+@hw.command()
+@click.option('--env', '-e', multiple=True)
+@click.option('--auth-token', envvar="AUTH_TOKEN", default="NONE")
+@click.argument('image')
+def create(**kwargs):
+    api = EndpointAPI(requests, kwargs.get("auth_token"))
+    data = dict(image=kwargs.get("image"), env="{}")
+    response = api.post(data=data)
+    if response.status_code == 401:
+        print("Authentication Failed")
+    if response.status_code == 429:
+        print("No more endpoints left")
+    if response.status_code == 200:
+        print("Will be deployed at http://{}.run.{}".format(response.json()["runner-name"], os.environ.get("HW_API")))
+        print("Give it some time to pull your image")
+    if response.status_code != 200:
+        sys.exit(1)
+
+
+@hw.command()
+@click.option('--auth-token', envvar="AUTH_TOKEN", default="NONE")
+@click.argument('endpoint-name')
+def remove(**kwargs):
+    api = EndpointAPI(requests, kwargs.get("auth_token"))
+    respone = api.delete(kwargs.get("endpoint_name"))
+
+if __name__ == '__main__':
+    hw()
---
+++ tests.py
@@ -0,0 +1,74 @@
+import os
+import requests
+from endpoint import cli
+import click
+from click.testing import CliRunner
+import requests_mock
+import unittest
+from mock import patch
+
+@requests_mock.Mocker()
+class CliTestCase(unittest.TestCase):
+    def setUp(self):
+        os.environ["HW_API"] = "mock.mock"
+        super().setUp()
+        self.runner = CliRunner()
+
+    def test_unauthenticated(self, m):
+        m.post("http://mock.mock/endpoints", status_code=401)
+        result = self.runner.invoke(cli.hw, ["create", "ubuntu"], catch_exceptions=False)
+        self.assertIn("Authentication Failed", result.output)
+        self.assertEqual(result.exit_code, 1)
+        self.assertTrue(m.called)
+
+    def test_no_more_resources(self, m):
+        m.post("http://mock.mock/endpoints", status_code=429)
+        result = self.runner.invoke(cli.hw, ["create", "ubuntu"])
+        self.assertIn("No more endpoints left", result.output)
+        self.assertEqual(result.exit_code, 1)
+        self.assertTrue(m.called)
+
+    def test_create(self, m):
+        m.post("http://mock.mock/endpoints", status_code=200)
+        result = self.runner.invoke(cli.hw, ["create", "ubuntu"], catch_exceptions=False)
+        self.assertEqual(result.exit_code, 0)
+        self.assertIn("Will be deployed at http://", result.output)
+        self.assertIn("Give it some time to pull your image", result.output)
+        self.assertTrue(m.called)
+
+    def test_delete(self, m):
+        m.delete("http://mock.mock/endpoints/ice-cream", status_code=200)
+        result = self.runner.invoke(cli.hw, ["remove", "ice-cream"], catch_exceptions=False)
+        self.assertEqual(result.exit_code, 0)
+        self.assertTrue(m.called)
+
+    def test_create_with_env(self, m):
+        m.post("http://mock.mock/endpoints", status_code=200)
+        result = self.runner.invoke(cli.hw, ["create", "ubuntu", "-e TEST=True", "-e DEBUG=False"])
+        self.assertIn("Will be deployed at http://", result.output)
+
+    @patch("endpoint.cli.EndpointAPI.__init__")
+    def test_create_uses_auth_token(self, m, e):
+        result = self.runner.invoke(cli.hw, ["create", "ubuntu", "--auth-token=123"])
+        e.assert_called_with(requests, "123")
+
+    @patch("endpoint.cli.EndpointAPI.__init__")
+    def test_delete_uses_auth_token(self, m, e):
+        result = self.runner.invoke(cli.hw, ["remove", "ubuntu", "--auth-token=123"])
+        e.assert_called_with(requests, "123")
+
+    @patch("endpoint.cli.EndpointAPI.__init__")
+    def test_create_uses_auth_token__env(self, m, e):
+        os.environ["AUTH_TOKEN"] = "123"
+        result = self.runner.invoke(cli.hw, ["create", "ubuntu"])
+        e.assert_called_with(requests, "123")
+
+    @patch("endpoint.cli.EndpointAPI.__init__")
+    def test_delete_uses_auth_token__env(self, m, e):
+        os.environ["AUTH_TOKEN"] = "123"
+        result = self.runner.invoke(cli.hw, ["remove", "ubuntu"])
+        e.assert_called_with(requests, "123")
+
+
+if __name__ == "__main__":
+    unittest.main()
